11b0b01f70f509128863904dd83bed2ae2255b20
FinnyBoi789 <f.lyon-crawford@rgu.ac.uk>
2026-03-16 22:43:47 +0000

    Left hand animations
    Left hand animations working, grab animation with sword not working properly.

--- Assets/Scripts/GrabAnimator.cs
+++ Assets/Scripts/GrabAnimator.cs
@@ -2,21 +2,25 @@ using UnityEngine;
 
 public class GrabAnimator : MonoBehaviour
 {
-    [SerializeField] private MeshRenderer handMesh;
+    [SerializeField] private SkinnedMeshRenderer handMeshR;
+    [SerializeField] private SkinnedMeshRenderer handMeshL;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        handMesh.enabled = false;
+        handMeshR.enabled = false;
+        handMeshL.enabled = false;
     }
 
     public void selectEnter()
     {
-        handMesh.enabled = true;
+        handMeshR.enabled = true;
+        handMeshL.enabled = true;
     }
 
     public void selectExit()
     {
-        handMesh.enabled = false;
+        handMeshR.enabled = false;
+        handMeshL.enabled = false;
     }
 }
